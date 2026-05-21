83608566865eb8b7fd9ab007fedee1bd2bbfd8c6
algoccs <algodemoacc@gmail.com>
2026-05-21 00:22:04 -0400

    Refactor database.py for clarity and correctness
    Refactor database.py to correct comments, enforce foreign key constraints, and improve table creation queries. Also, fix SQL syntax in drop_tables and add missing newline at end of file.

--- database.py
+++ database.py
@@ -3,43 +3,47 @@ DB = 'quises.sqlite3'
 conn = None
 cursor = None
 
+
 def open():
     global conn, cursor
     conn = sqlite3.connect(DB)
     cursor = conn.cursor()
-    #CONFIGURACION ADICIONAL
-    cursor.execute('PRAGMA foreign_key=on')
+    # CONFIGURACION ADICIONAL
+    cursor.execute('PRAGMA foreign_keys=on')
+
 
 def close():
     cursor.close()
     conn.close()
 
+
 def execute_query(query):
     cursor.execute(query)
     conn.commit()
 
+
 def create_tables():
-    queries =[
+    queries = [
         '''CREATE TABLE IF NOT EXISTS quiz (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL);''',
-    
-    '''CREATE TABLE IF NOT EXISTS question (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    question_name TEXT NOT NULL,
-    correct VARCHAR (100) NOT NULL,
-    wrong1 VARCHAR (100) NOT NULL,
-    wrong2 VARCHAR (100) NOT NULL,
-    wrong3 VARCHAR (100) NOT NULL);''',
-    
-    '''CREATE TABLE IF NOT EXISTS quiz_content (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    quiz_id INTEGER NOT NULL,
-    question_id INTEGER NOT NULL,
-    FOREIGN KEY (quiz_id) REFERENCES quiz(id),
-    FOREIGN KEY (question_id) REFERENCES question (id));'''
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL);''',
+
+        '''CREATE TABLE IF NOT EXISTS question (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            question_name TEXT NOT NULL,
+            correct VARCHAR (100) NOT NULL,
+            wrong1 VARCHAR (100) NOT NULL,
+            wrong2 VARCHAR (100) NOT NULL,
+            wrong3 VARCHAR (100) NOT NULL);''',
+
+        '''CREATE TABLE IF NOT EXISTS quiz_content (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            quiz_id INTEGER NOT NULL,
+            question_id INTEGER NOT NULL,
+            FOREIGN KEY (quiz_id) REFERENCES quiz(id) ON DELETE CASCADE,
+            FOREIGN KEY (question_id) REFERENCES question (id) ON DELETE CASCADE);'''
     ]
-    
+
     open()
     for q in queries:
         execute_query(q)
@@ -50,21 +54,28 @@ def add_quises():
     quizes = [
         ('Propio juego', ),
         ('¿Quién quiere ser millonario?', ),
-        ('El más inteligente', )     
+        ('El más inteligente', )
     ]
     open()
-    cursor.executemany('INSERT INTO quiz (name) VALUES (?);',quizes)
+    cursor.executemany('INSERT INTO quiz (name) VALUES (?);', quizes)
     conn.commit()
     close()
 
+
 def add_question():
     questions = [
-        ('¿Cuántos meses en un año tienen 28 días?', 'Todos', 'Uno', 'Ninguno','Dos'),
-        ('¿Qué aspecto tendrá el acantilado verde si se cae en el Mar Rojo?', 'Mojado', 'Rojo', 'No cambiará', 'Púrpura'),
-        ('¿Con qué mano es mejor mezclar el té?', 'Con una cuchara', 'Derecha', 'Izquierda', 'Cualquiera'),
-        ('¿Qué no tiene longitud, profundidad, ancho, o altura pero puede medirse?', 'Tiempo', 'Estupidez', 'El mar','Aire'),
-        ('¿Cuándo es posible sacar a-gua con una red?', 'Cuando el agua está congelada', 'Cuando no hay peces', 'Cuando los peces de colores nadan lejos', 'Cuando la red se rompe'),
-        ('¿Qué es más grande que un elefante y no pesa nada?', 'La sombra de un elefante','Un globo','Un paracaídas', 'Una nube')
+        ('¿Cuántos meses en un año tienen 28 días?',
+         'Todos', 'Uno', 'Ninguno', 'Dos'),
+        ('¿Qué aspecto tendrá el acantilado verde si se cae en el Mar Rojo?',
+         'Mojado', 'Rojo', 'No cambiará', 'Púrpura'),
+        ('¿Con qué mano es mejor mezclar el té?',
+         'Con una cuchara', 'Derecha', 'Izquierda', 'Cualquiera'),
+        ('¿Qué no tiene longitud, profundidad, ancho, o altura pero puede medirse?',
+         'Tiempo', 'Estupidez', 'El mar', 'Aire'),
+        ('¿Cuándo es posible sacar a-gua con una red?', 'Cuando el agua está congelada',
+         'Cuando no hay peces', 'Cuando los peces de colores nadan lejos', 'Cuando la red se rompe'),
+        ('¿Qué es más grande que un elefante y no pesa nada?',
+         'La sombra de un elefante', 'Un globo', 'Un paracaídas', 'Una nube')
     ]
     open()
     cursor.executemany('''INSERT INTO question (question_name, correct,         wrong1, wrong2, wrong3)
@@ -72,12 +83,14 @@ def add_question():
     conn.commit()
     close()
 
+
 def drop_tables():
     open()
     tables = ['quiz_content', 'quiz', 'question']
     for table in tables:
-        execute_query(f'DROP TABLE IF EXISTS {table}') 
+        execute_query(f'DROP TABLE IF EXISTS {table};')
     close()
+
 
 def create_links():
     links = []
@@ -98,6 +111,7 @@ def create_links():
         conn.commit()
         close()
 
+
 def show_tables():
     tables = ['quiz_content', 'quiz', 'question']
     open()
@@ -113,6 +127,8 @@ def show_tables():
         except sqlite3.DatabaseError as error:
             print('SE ENCONTRO EL SIGUIENTE ERROR:', error)
 
+    close()
+
 
 def fetch_data(sql, data=None):
     open()
@@ -125,5 +141,3 @@ def fetch_data(sql, data=None):
 
     return result
 
-def get_next_question(id_question=0, quiz_id):
-    pass
