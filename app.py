965d54cec0f96df46f925e9a77229262671a1f34
algoccs <algodemoacc@gmail.com>
2026-06-18 18:18:34 -0400

    Refactor quiz logic and update session handling

--- app.py
+++ app.py
@@ -1,82 +1,90 @@
 from flask import Flask, redirect, url_for, session, request, render_template
 from database import get_next_question, get_quises
-from random import *
+from random import shuffle
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'SuperClaveSecreta'
+app.config['SECRET_KEY'] = 'NoTieneClave'
+
 
 def start_quiz(quiz_id):
-    session['quiz'] = quiz_id
+    # Convertimos explicitamente a int (para mantener la integridad del dato acorde a la DB)
+    session['quiz'] = int(quiz_id)
     session['prev_question'] = 0
-    session['correct'] = 0
-    session['totals'] = 0
+    session['correctas'] = 0
+    session['totales'] = 0
+
 
 def end_quiz():
     session.clear()
 
 def check_answer():
-    answer = request.form.get('ans_text')
-    correct_answer = session.get('prev_correct_answer')
+    resp_usuario = request.form.get('ans_text')
+    resp_correcta = session.get('last_correct')
 
-    if answer:
-        session['totals'] += 1
-        if answer == correct_answer:
-            session['correct'] += 1
+    if resp_usuario:
+        session['totales'] += 1
+        if resp_usuario == str(resp_correcta):
+            session['correctas'] += 1
 
-def cal_stats(correct, totals):
-    if totals > 0:
-        return round((correct / totals) * 100, 2)
+def calc_stats(total, correct):
+    if total:
+        return round((correct / total) * 100, 2)
     return 0
+
 
 def index():
     if request.method == 'GET':
-        start_quiz(-1)
+        end_quiz()
         quises_list = get_quises()
         return render_template('index.html', quises=quises_list)
     else:
-        quiz = request.form.get('quiz')
-        start_quiz(quiz)
-        return redirect(url_for('test'))
+        quiz_id = request.form.get('quiz')
+        if quiz_id:
+            start_quiz(quiz_id)
+            return redirect(url_for('test'))
+        return redirect(url_for('index'))
+
 
 def test():
-    if 'quiz' not in session:
+    # si no hay un quiz activo en la session, regresa al index
+    if 'quiz' not in session or 'prev_question' not in session:
         return redirect(url_for('index'))
 
     if request.method == 'POST':
         check_answer()
 
     result = get_next_question(session['prev_question'], session['quiz'])
-    question_name = result[1]
-    options = list(result[2:6]) # BARAJAR ESA LISTA CON SHUFFE
-    # shuffle(options)
 
-    if result is None or result == 1:
+    if result is None or result == 0:
         return redirect(url_for('result'))
-
-    session['prev_correct_answer'] = result[2]
+    
     session['prev_question'] = result[0]
+    session['last_correct'] = result[2]
 
-    return render_template('test.html',pregunta=question_name, opciones=options)
+    respuestas = list(result[2:6]) # MEZCLEN LAS RESPUESTAS
+    shuffle(respuestas)
+
+    print(session.get('totales', 'correctas'))
+    return render_template('test.html', pregunta=result[1], opciones=respuestas)
+    
 
 
 def result():
-    if 'totals' not in session:
-        end_quiz()
+    if 'totales' not in session:
         return redirect(url_for('index'))
-
-    totals = session['totals']
-    correct = session['correct']
-    incorrect = totals - correct
-    percentage = cal_stats(correct, totals)
+    
+    total = session.get('totales', 0)
+    correct = session.get('correctas', 0)
+    incorrect = total - correct
+    percent = calc_stats(total, correct)
 
     return render_template('result.html',
-                            totales=totals,
+                            totales=total,
                             correctas=correct,
                             incorrectas=incorrect,
-                            porcentaje=percentage
-                            )
-    
+                            porcentaje= percent)
 
+# Configuración de Rutas
 app.add_url_rule('/', 'index', index, methods=['GET', 'POST'])
 app.add_url_rule('/test', 'test', test, methods=['GET', 'POST'])
 app.add_url_rule('/result', 'result', result)
